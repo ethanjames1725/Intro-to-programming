db1040118a6388209eaeb15a692e73a5e61f55a8
Ethan <ethanjames1725@gmail.com>
2026-05-05 13:15:49 +0200

    Add files via upload

---
+++ Classwork/Book_2/Chapter 3/chapter3.js
@@ -0,0 +1,8 @@
+var toName = "Ethan";
+var fromName = "The Grammy Awards";
+var letterBody = "We are pleased to inform you that your song, 'Can\'t Stop Coding!,' has been voted the Best Song of All Time by the awarding committee.";
+
+document.write("Dear " + toName + ",<br><br>");
+document.write(letterBody + "<br><br>");
+document.write("Sincerely,<br>");
+document.write(fromName);
