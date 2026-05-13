5cfee6e91f7fa73906a49be7b89f729c36724e6b
Ethan <ethanjames1725@gmail.com>
2026-05-13 13:37:12 +0200

    Add files via upload

---
+++ Classwork/Book_2/Chapter 17/loops.js
@@ -0,0 +1,22 @@
+// for (var counter = 0; counter < 500; counter++) {
+//     console.log(counter + ": Hello, JavaScript!");
+// }
+// for (var i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+// alert("Blass Off!");
+
+// var myFriends = ["Agatha", "Agnes", "Jermaine", "Jack"];
+
+// for (var i = 0; i < myFriends.length; i++) {
+//     alert(myFriends[i] + " is my friend.");
+// }
+
+// alert(Math.random());
+// alert(Math.random() * 11);
+// alert(Math.floor(Math.random() * 11));
+// alert(Math.floor(Math.random() * (1000 - 100)) + 100);
+
+// var myFriends = ["Agatha", "Agnes", "Jermaine", "Jack"];
+// var randomFriend = Math.floor(Math.random() * myFriends.length);
+// alert(myFriends[randomFriend] + " is my friend.");
